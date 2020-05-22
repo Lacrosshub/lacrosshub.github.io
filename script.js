d748b5b76e2c270854893e730e233abc27fc56aa
Lacrosshub <64489403+Lacrosshub@users.noreply.github.com>
2020-05-22 10:48:09 -0700

    LacrosseHubFinal

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 var options = {
-  bottom: '64px', // default: '32px'
-  right: 'unset', // default: '32px'
-  left: '32px', // default: 'unset'
+  bottom: '150px',// default: '32px'
+  right: '32px', // default: '32px'
+  left: 'unset', // default: 'unset'
   time: '0.5s', // default: '0.3s'
   mixColor: '#fff', // default: '#fff'
   backgroundColor: '#fff',  // default: '#fff'
   buttonColorDark: '#100f2c',  // default: '#100f2c'
   buttonColorLight: '#fff', // default: '#fff'
   saveInCookies: false, // default: true,
-  label: '🌓', // default: ''
+  label: 'Dark', // default: ''
   autoMatchOsTheme: true // default: true
 }
 
 const darkmode = new Darkmode(options);
 darkmode.showWidget();
+
+//*code above is from Darkmode Js. URL: https://darkmodejs.learn.uno/ */
